4c4b14c0a02221c2183752f50c37936834f28903
31648 <2113879@talnet.nl>
2020-11-26 09:52:21 +0100

    Totaalknop

---
+++ src/H010/test.java
@@ -0,0 +1,17 @@
+package H010;
+
+import java.awt.*;
+import java.applet.*;
+
+
+public class test extends Applet {
+
+    public void init() {
+    }
+
+
+    public void paint(Graphics g) {
+        g.drawString("" + 8 + " is groter dan " + 5 + ": " + (8 > 5), 50, 60);
+        g.drawString("" + 8 + " is kleiner dan " + 5 + ": " + (8 < 5), 50, 80);
+    }
+}
