6319e7e632a03a39a3d627660b2b3adcb3532d28
31648 <2113879@talnet.nl>
2020-12-11 10:49:46 +0100

    h0121

--- src/H011/h0112.java
+++ src/H011/h0112.java
@@ -1,4 +1,24 @@
 package H011;
 
-public class h0112 {
-}
+
+import java.awt.*;
+import java.applet.*;
+
+
+    public class h0112 extends Applet {
+
+        public void init() {}
+
+        public void paint(Graphics g) {
+            setBackground(Color.gray);
+            int teller;
+            int y = 0;
+
+            for(teller = 10; teller < 21; teller++) {
+                y += 20;
+
+                g.drawString("" + teller, 305, y );
+            }
+        }
+    }
+
