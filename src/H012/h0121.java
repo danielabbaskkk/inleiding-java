1b136251a8625a5b3d27b9d2d1e5620e4d7cc845
31648 <2113879@talnet.nl>
2020-12-11 08:48:40 +0100

    h0121

--- src/H012/h0121.java
+++ src/H012/h0121.java
@@ -1,16 +1,21 @@
-package H012;
-
-import java.applet.Applet;
+H012
+import java.awt.*;
+import java.applet.*;
 
 public class h0121 extends Applet {
-    public void init(){
-        salaris[0] = 100;
-        salaris[1] = 200;
-        salaris[2] = 300;
-        salaris[3] = 400;
-        salaris[4] = 500;
-        (int teller = 0; teller < 5; teller ++)
-        salaris[teller] = 100 * (teller + 1);
+    double salaris[];
+
+    public void init() {
+        salaris = new double [5];
+
+        for (int teller = 0; teller < salaris.length; teller ++) {
+            salaris[teller] = 100 * teller + 100;
+        }
     }
 
+    public void paint(Graphics g) {
+        for (int teller = 0; teller < salaris.length; teller ++) {
+            g.drawString("" + salaris[teller], 50, 20 * teller + 20);
+        }
+    }
 }
