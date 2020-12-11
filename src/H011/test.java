3a13b1e8108d00a19cface27f7060345487a84c8
31648 <2113879@talnet.nl>
2020-12-11 11:24:24 +0100

    h0121

---
+++ src/H011/test.java
@@ -0,0 +1,24 @@
+package H011;
+
+
+    import java.awt.*;
+import java.applet.*;
+
+
+    public class test extends Applet {
+
+        public void init() {}
+
+        public void paint(Graphics g) {
+            int teller = 0;
+            int y = 0;
+
+            while(teller < 10) {
+                y += 20;
+                g.drawLine(50 , y, 300, y );
+                g.drawString("" + teller, 305, y );
+                teller++;
+            }
+        }
+    }
+
